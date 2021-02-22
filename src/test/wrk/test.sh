69523365cc391ba9def0e5b9dfa5831cfd41b149
Roman Komov <rkomov@fusionconnect.com>
2021-02-22 03:07:28 +0300

    fix path

--- src/test/wrk/test.sh
+++ src/test/wrk/test.sh
@@ -1,4 +1,4 @@
-docker run --rm -v /media/roman/projects/otus/hl/social-network/src/test/wrk:/tmp/ --network host  williamyeh/wrk \
--t$1 -c$1 -d60 -s /tmp/$2 --latency \
+docker run --rm -v $(pwd):/tmp/ --network host  williamyeh/wrk \
+-t$1 -c$1 -d120 -s /tmp/$2 --latency \
 http://localhost:8080
 #http://localhost:8080/api/profiles
